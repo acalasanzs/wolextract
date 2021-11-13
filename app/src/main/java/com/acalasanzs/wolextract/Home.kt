a8cc5749de30df1e5a3c2b8323507ee17b1777ff
Albert Calasanz <acalasanzs@gmail.com>
2021-11-13 07:01:08 +0100

    dum

--- app/src/main/java/com/acalasanzs/wolextract/Home.kt
+++ app/src/main/java/com/acalasanzs/wolextract/Home.kt
@@ -1,10 +1,14 @@
 package com.acalasanzs.wolextract
 
+import android.animation.ValueAnimator
+import android.graphics.Interpolator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AccelerateInterpolator
+import android.widget.Button
 import android.widget.NumberPicker
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
@@ -22,10 +26,22 @@ class Home : Fragment() {
         binding.trivia.setOnClickListener { view: View ->
             Navigation.findNavController(view).navigate(R.id.action_home3_to_triviaFragment)
         }
+        binding.wallpaper.setOnClickListener { view: View ->
+            Navigation.findNavController(view).navigate(R.id.action_home3_to_wallpaper2)
+        }
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         var book: NumberPicker = requireView().findViewById(R.id.book)
+        val bgButton: Button = requireView().findViewById(R.id.wallpaper)
+        val valueAnimator = ValueAnimator.ofFloat(250f,50f,200f)
+        valueAnimator.addUpdateListener {
+            val value = it.animatedValue as Float
+            bgButton.translationX = value
+        }
+        valueAnimator.interpolator = AccelerateInterpolator(1.5f)
+        valueAnimator.duration = 780
+        valueAnimator.start()
         book.minValue = 0
         book.maxValue = 1
         book.displayedValues = arrayOf("Genesis","Éxodo")
